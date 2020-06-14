ebe85d4b42fd0be1014127cabb1ed9b3246f85c5
asgaraliyev <asgaraliyev2001@gmail.com>
2020-06-14 19:41:33 +0400

    jquery.js syntaxt error oldu

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,45 +1,46 @@
 jQuery(document).ready(function ($) {
+    $.getJSON('https://ipapi.co/json/', function (data) {
+
+        isCurrencyChanged = Cookies.get('isCurrencyhanged')
+        if (isCurrencyChanged) {
+            itisLang = Cookies.get('currency')
+        } else {
             $.getJSON('https://ipapi.co/json/', function (data) {
-
-                isCurrencyChanged = Cookies.get('isCurrencyhanged')
-                if (isCurrencyChanged) {
-                    itisLang = Cookies.get('currency')
-                } else {
-                    $.getJSON('https://ipapi.co/json/', function (data) {
-                        var currency = $("#money .currency").val();
-                        var res = currency.split("/");
-                        Cookies.set("currency", data.currency)
-                        Cookies.set("currencyvalue", res[1])
-                        Cookies.set("isCurrencyhanged", true)
-                    });
-                }
-
-
-            });
-
-            function filterSelection(c) {
-                if (c == "all") {
-                    $("#cars-lits .sport").fadeIn();
-                    $("#cars-lits .business").fadeIn();
-                    $("#cars-lits .full-size").fadeIn();
-                    $("#cars-lits .minivan").fadeIn();
-                    $("#cars-lits .economy").fadeIn();
-                } else {
-                    $("#cars-lits .sport").fadeOut();
-                    $("#cars-lits .business").fadeOut();
-                    $("#cars-lits .full-size").fadeOut();
-                    $("#cars-lits .minivan").fadeOut();
-                    $("#cars-lits .economy").fadeOut();
-                    $("#cars-lits ." + c).fadeIn();
-                }
-            }
-
-
-            function changethemoney() {
                 var currency = $("#money .currency").val();
                 var res = currency.split("/");
-                Cookies.set("currency", res[0])
+                Cookies.set("currency", data.currency)
                 Cookies.set("currencyvalue", res[1])
                 Cookies.set("isCurrencyhanged", true)
-                window.location.href = window.location.href;
-            }
+            });
+        }
+
+
+    });
+});
+
+function filterSelection(c) {
+    if (c == "all") {
+        $("#cars-lits .sport").fadeIn();
+        $("#cars-lits .business").fadeIn();
+        $("#cars-lits .full-size").fadeIn();
+        $("#cars-lits .minivan").fadeIn();
+        $("#cars-lits .economy").fadeIn();
+    } else {
+        $("#cars-lits .sport").fadeOut();
+        $("#cars-lits .business").fadeOut();
+        $("#cars-lits .full-size").fadeOut();
+        $("#cars-lits .minivan").fadeOut();
+        $("#cars-lits .economy").fadeOut();
+        $("#cars-lits ." + c).fadeIn();
+    }
+}
+
+
+function changethemoney() {
+    var currency = $("#money .currency").val();
+    var res = currency.split("/");
+    Cookies.set("currency", res[0])
+    Cookies.set("currencyvalue", res[1])
+    Cookies.set("isCurrencyhanged", true)
+    window.location.href = window.location.href;
+}
